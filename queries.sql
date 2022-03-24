1d453346466383b58d309da0b8bb1744f14265ec
Jamaine Smith <smithjamainep@gmail.com>
2022-03-24 17:31:35 -0500

    Final Commit

--- queries.sql
+++ queries.sql
@@ -1,4 +1,21 @@
 -- Part 1: Test it with SQL
+select COLUMN_NAME, DATA_TYPE
+from INFORMATION_SCHEMA.COLUMNS
+where TABLE_NAME='job'
+-- Part 2: Test it with SQL
+
+SELECT name FROM employer WHERE location = "St. Louis City";
+
+-- Part 3: Test it with SQL
+
+DROP TABLE job;
+
+-- Part 4: Test it with SQL
+
+SELECT * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;-- Part 1: Test it with SQL
 
 -- Part 2: Test it with SQL
 
